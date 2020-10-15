c477d361af897060d0bea505748efadcbc846035
secretmens <70989987+secretmens@users.noreply.github.com>
2020-10-15 03:11:20 -0300

    Add files via upload

---
+++ farupee.py
@@ -0,0 +1,141 @@
+import os, sys, time, random
+os.system("pkg install python && apt-get install python")
+os.system("pkg install python2 && apt-get install python2")
+os.system("pkg install php && apt-get install php")
+os.system("pkg install clang && pkg install wget && pkg install sudo")
+os.system("apt-get install clang && apt-get install wget && apt-get install sudo")
+os.system("pkg install tor && pkg install perl && pkg install curl")
+os.system("apt-get install tor && apt-get install perl && apt-get install curl")
+os.system("pkg install git && apt-get install git")
+os.system("pkg install bash && apt-get install bash")
+os.system("git clone https://github.com/Cesar-Hack-Gray/FotoSploit")
+os.system("git clone https://github.com/An0nUD4Y/SocialFish.git")
+os.system("git clone https://github.com/evait-security/weeman.git")
+os.system("git clone https://github.com/Cesar-Hack-Gray/SocialSploit")
+os.system("git clone https://github.com/Cesar-Hack-Gray/FotoSploit")
+os.system("git clone https://github.com/DarkSecDevelopers/HiddenEye.git")
+os.system("git clone https://github.com/suljot/shellphish")
+os.system("git clone https://github.com/htr-tech/zphisher")
+os.system("git clone https://github.com/xHak9x/SocialPhish")
+os.system("git clone https://github.com/htr-tech/nexphisher")
+
+
+print("	\033[1;32m   █████▒▄▄▄       ██▀███   █    ██  ██▓███  ▓█████ 	\033[1;32m ")
+print("	\033[1;32m ▓██   ▒▒████▄    ▓██ ▒ ██▒ ██  ▓██▒▓██░  ██▒▓█   ▀ 	\033[1;32m ")
+print("	\033[1;32m ▒████ ░▒██  ▀█▄  ▓██ ░▄█ ▒▓██  ▒██░▓██░ ██▓▒▒███   	\033[1;32m ")
+print("\033[1;31m                       secretmens hacking    \033[1;31m")
+print("	\033[1;32m ░▓█▒  ░░██▄▄▄▄██ ▒██▀▀█▄  ▓▓█  ░██░▒██▄█▓▒ ▒▒▓█  ▄ 	\033[1;32m ")
+print("	\033[1;32m ░▒█░    ▓█   ▓██▒░██▓ ▒██▒▒▒█████▓ ▒██▒ ░  ░░▒████▒	\033[1;32m ")
+print("	\033[1;32m  ▒ ░    ▒▒   ▓▒█░░ ▒▓ ░▒▓░░▒▓▒ ▒ ▒ ▒▓▒░ ░  ░░░ ▒░ ░	\033[1;32m ")
+print("	\033[1;32m  ░       ▒   ▒▒ ░  ░▒ ░ ▒░░░▒░ ░ ░ ░▒ ░      ░ ░  ░	\033[1;32m ")
+print("	\033[1;32m  ░ ░     ░   ▒     ░░   ░  ░░░ ░ ░ ░░          ░   	\033[1;32m ")
+print("	\033[1;32m              ░  ░   ░        ░                 ░  ░	\033[1;32m ")
+print("\033[1;36m caso de algum erro escreva python3 farupee.py novamente caso voce esteja no celular,nós da secretmens recomendamos executar nossa ferramenta na horizontal ligue a rotação do celular e o vire na horizontal\033[1;36m")
+nome=input("nome:")
+print()
+print("Bem vindo ",nome)
+print("\033[1;31mAPERTE 1 PARA COMEÇAR A SESSÃO\033[1;31m ")
+
+
+numero=int(input("\033[1;34mEntre com o número: \033[1;34m"))
+if numero<2:
+	print("vamos começar")
+	
+else:
+  print("O número esta incorreto.")
+  numero=int(input("Entre com o número: "))
+  print("O número esta incorreto.")
+print("\033[1;33m ██████╗ ██╗  ██╗██╗███████╗██╗  ██╗██╗███╗   ██╗ ██████╗ ███████╗ \033[1;33m")
+print("\033[1;33m ██╔══██╗██║  ██║██║██╔════╝██║  ██║██║████╗  ██║██╔════╝ ██╔════╝ \033[1;33m")
+print("\033[1;33m ██████╔╝███████║██║███████╗███████║██║██╔██╗ ██║██║  ███╗███████╗ \033[1;33m")
+print("\033[1;33m ██╔═══╝ ██╔══██║██║╚════██║██╔══██║██║██║╚██╗██║██║   ██║╚════██║ \033[1;33m")
+print("\033[1;33m ██║     ██║  ██║██║███████║██║  ██║██║██║ ╚████║╚██████╔╝███████║ \033[1;33m")
+print("\033[1;33m ╚═╝     ╚═╝  ╚═╝╚═╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝ \033[1;33m")
+print("(1)Termux Phishings")
+print("\033[1;32m(2)Kali linux Phishings\033[1;32m")
+op=input("input the option:")
+if (op =='1'):
+                print("\033[1;33m(1) SocialPhish\033[1;33m")
+                print("\033[1;33m(2) Weeman\033[1;33m")
+                print("\033[1;33m(3) SocialSploit\033[1;33m")
+                print("\033[1;33m(4) Hidden Eye\033[1;33m")
+                print("\033[1;33m(5) FotoSploit\033[1;33m")
+                print()
+                pr = input("escolha o numero da ferramenta:")
+                while pr not in '12345':
+                    print()
+                    pr = input("digite corretamenta:")
+                if (pr == '1'):
+                    print("voce escolheu SocialPhish")
+                    print()
+                    os.system("cd SocialFish && chmod +x * && pip2 install -r requirements.txt && python2 SocialFish.py")
+
+                else:
+                    if (pr == '1'):
+                        print()
+                        print("voce escolheu SocialPhish")
+                    else:
+                        if (pr == '2'):
+                            print()
+                            print("voce escolheu Weeman")
+                            print()
+                            os.system("cd weeman && python2 weeman.py")
+
+                        else:
+                            if (pr == '3'):
+                                print()
+                                print("voce escolheu SocialSploit")
+                                print()
+                                os.system("chmod +x * && cd SocialSploit && chmod +x * && bash install.sh && ./Sploit")
+
+                            else:
+                                if (pr == '4'):
+                                    print()
+                                    print("voce escolheu Hidden Eye")
+                                    print()
+                                    os.system("pkg install git python php curl openssh grep && pip3 install wget && chmod 777 HiddenEye && cd HiddenEye && ./HiddenEye.py")
+
+                                else:
+                                    if (pr == '5'):
+                                        print()
+                                        print("voce escolheu FotoSploit")
+                                        print()
+                                        os.system("pkg install -y php && pkg install -y python2 && cd $HOME && cd FotoSploit && bash install.sh && ./FotoSploit")
+if (op == '2'):
+            print()
+            print("(1) shellphish")
+            print("(2) zphisher  ")
+            print("(3) socialphish")
+            print("(4) nexphisher")
+            cu = input("escolha o numero:")
+            print()
+            while cu not in '1234':
+                print()
+                cu = input("digite corretamente:")
+            if (cu == '1'):
+                print()
+                print("voce escolheu shellphish")
+                os.system("cd shellphish && bash shellphish.sh")
+            else:
+                if (cu == '2'):
+                    print()
+                    print("voce escolheu zphisher")
+                    print()
+                    os.system("apt update ; apt install git curl php openssh -y ; cd zphisher ; bash zphisher.sh")
+
+                else:
+                    if(cu=='3'):
+                        print("voce escolheu SocialPhish")
+                        print()
+                        os.system("cd SocialPhish && chmod +x socialphish.sh &&./socialphish.sh")
+
+                    else:
+                        if(cu=='4'):
+                            print("voce escolheu nexphisher")
+                            print()
+                            os.system("apt update ; apt install git -y ; cd nexphisher ; bash setup ; bash nexphisher")
+
+
+
+
+
